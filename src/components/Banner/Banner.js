76e480d118e760057448f4c35981d38e727e0ac9
Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>
2024-01-02 04:59:39 -0300

    Add banner component
    Signed-off-by: Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="https://placehold.co/1200x300.jpg" />
+        </header>
+    );
+}
+
+export default Banner;
