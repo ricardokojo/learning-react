9ac887df6ea647b7bbd49d318c274c5c5c317a6c
Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>
2024-01-05 02:12:27 -0300

    Add submit handler
    Signed-off-by: Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Form from './components/Form/Form';
 
 function App() {
+  const [members, setMembers] = useState([]);
+
+  const onAddMember = (member) => {
+    setMembers([...members, member]);
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Form />
+      <Form onAddMember={onAddMember} />
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,7 +3,7 @@ import InputText from "../InputText/InputText";
 import Select from "../Select/Select";
 import './Form.css';
 
-function Form() {
+function Form(props) {
     const teams = [
         'Backend',
         'Frontend',
@@ -14,8 +14,14 @@ function Form() {
         'Inovacão'
     ];
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        props.onAddMember(Object.fromEntries(formData.entries()));
+    }
+
     return (
-        <form className="custom-form">
+        <form className="custom-form" onSubmit={onSubmit}>
             <h2>Preencha os dados para criar o card do colaborador.</h2>
             <InputText name="Nome" placeholder="Digite seu nome" required={true} />
             <InputText name="Cargo" placeholder="Digite seu cargo" required={true} />
