3e62ce2c4a8d9ba853badefd80dc1b271a911754
Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>
2024-01-08 00:20:54 -0300

    Fix team member listing style
    Signed-off-by: Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -5,14 +5,19 @@ function Team(props) {
     return (
         <section className='team' style={{ backgroundColor: props.primary }}>
             <h3 style={{ borderBottomColor: props.secondary }}>{props.name}</h3>
+            <ul>
             {props.members
                 .filter(member => member.team === props.slug)
-                .map(member => <Card
-                    color={props.secondary}
-                    name={member.name}
-                    imageUrl={member.imageUrl}
-                    position={member.position}
-                />)}
+                .map(member => 
+                <li>
+                    <Card
+                        color={props.secondary}
+                        name={member.name}
+                        imageUrl={member.imageUrl}
+                        position={member.position}
+                    />
+                </li>)}
+            </ul>
         </section>
     )
 }
