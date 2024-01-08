92227a1c50edf65b2330ae22744ea805048b502a
Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>
2024-01-07 23:31:12 -0300

    Add teams definition and mapping
    Signed-off-by: Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -10,11 +10,51 @@ function App() {
     setMembers([...members, member]);
   }
 
+  const teams = [
+    {
+      name: 'Backend',
+      primary: '#ffadad',
+      secondary: 'coral'
+    },
+    {
+      name: 'Frontend',
+      primary: '#ffd6a5',
+      secondary: 'orange'
+    },
+    {
+      name: 'Ciência de Dados',
+      primary: '#fdffb6',
+      secondary: 'gold'
+    },
+    {
+      name: 'DevOps',
+      primary: '#caffbf',
+      secondary: 'green'
+    },
+    {
+      name: 'UX e Design',
+      primary: '#9bf6ff',
+      secondary: 'skyblue'
+    },
+    {
+      name: 'Mobile',
+      primary: '#a0c4ff',
+      secondary: 'royalblue'
+    },
+    {
+      name: 'Inovacão',
+      primary: 'bdb2ff',
+      secondary: 'purple'
+    },
+  ];
+
   return (
     <div className="App">
       <Banner />
       <Form onAddMember={onAddMember} />
-      <Team team="Backend" />
+      {
+        teams.map(team => <Team key={team.name} team={team.name} primary={team.primary} secondary={team.secondary} />)
+      }
     </div>
   );
 }
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,8 +2,8 @@ import './Team.css';
 
 function Team(props) {
     return (
-        <section className='team'>
-            <h3>{props.team}</h3>
+        <section className='team' style={{ backgroundColor: props.primary }}>
+            <h3 style={{ borderBottomColor: props.secondary }}>{props.team}</h3>
         </section>
     )
 }
