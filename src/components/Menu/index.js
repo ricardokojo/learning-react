ec16f8290643f72d6f537f4e6cdf5848bf8f571d
Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>
2024-01-13 00:55:09 -0300

    Update menu to show current route
    Signed-off-by: Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,12 +1,22 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Menu.css';
 
 export default function Menu() {
     return (
         <header>
             <nav className='menu'>
-                <Link className='link' to="/">Início</Link>
-                <Link className='link' to="/about">Sobre mim</Link>
+                <NavLink
+                    className={({ isActive }) => `link ${isActive ? 'selected' : ''}`}
+                    to="/"
+                >
+                    Início
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) => `link ${isActive ? 'selected' : ''}`}
+                    to="/about"
+                >
+                    Sobre mim
+                </NavLink>
             </nav>
         </header>
     );
